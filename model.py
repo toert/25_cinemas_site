541c79050ee710286f90e46b5e0694f2ae4c1083
toert <pw177@mail.ru>
2017-01-15 18:29:07 +0300

    add API

--- model.py
+++ model.py
@@ -13,7 +13,7 @@ class Film(db.Model):
     genre = db.Column(db.String(80))
     image_url = db.Column(db.String(80))
     img_min = db.Column(db.String(80))
-    name = db.Column(db.String(80))
+    film_name = db.Column(db.String(80))
     rating = db.Column(db.String(80))
     rating_count = db.Column(db.Integer)
     review = db.Column(db.Text)
@@ -26,7 +26,7 @@ class Film(db.Model):
         self.genre = info_json['genre']
         self.image_url = info_json['image_url']
         self.img_min = info_json['img_min']
-        self.name = info_json['name']
+        self.film_name = info_json['name']
         self.rating = info_json['rating']
         self.rating_count = int(info_json['rating_count'])
         self.review = info_json['review']
--- server.py
+++ server.py
@@ -1,12 +1,15 @@
 from model import Film, db
 from flask import Flask, render_template
-from helpers import get_info_from_afisha, update_films_in_db, add_film_in_db
+from afisha import parse_afisha_list
+from helpers import get_info_from_afisha, update_films_in_db, add_film_in_db, get_info_about_film_from_db
+import re
+import json
 
 app = Flask(__name__)
 app.config.from_object('config')
 db.init_app(app)
 
-FILMS_AMOUNT = 8
+FILMS_AMOUNT = 10
 
 
 @app.route('/')
@@ -14,20 +17,34 @@ def films_list():
     query_all_films = get_info_from_afisha(FILMS_AMOUNT)
     info_about_all_films = []
     for film in query_all_films:
-        url_in_site = '/movie/{}'.format(film.id)
-        info_about_film = (film.name, film.rating, film.description,
-                           film.genre, url_in_site, film.img_min)
-        info_about_all_films.append(info_about_film)
+        info_about_all_films.append(get_info_about_film_from_db(film.id))
     return render_template('films_list.html', info=info_about_all_films)
 
 
-@app.route('/movie/<film_id>', methods=['GET'])
+@app.route('/movie/<int:film_id>', methods=['GET'])
 def film_page(film_id):
-    film = Film.query.filter_by(id=film_id).first()
-    info_about_film = (film.name, film.rating, film.rating_count, film.review,
-                       film.alt_name, film.genre, film.content, film.url, film.image_url)
-    return render_template('film_page.html', info=info_about_film)
+    return render_template('film_page.html', info=get_info_about_film_from_db(film_id))
+
+
+@app.route('/api/movies', methods=['GET'])
+def api_get_list():
+    list_of_films = {}
+    list_of_films['movies'] = []
+    for movie in parse_afisha_list(FILMS_AMOUNT):
+        list_of_films['movies'].append({'url': movie,
+                                        'id': re.findall(r'http://www.afisha.ru/movie/(\d+)/', movie)[0]})
+    return json.dumps(list_of_films)
+
+
+@app.route('/api/movies/<int:film_id>', methods=['GET'])
+def api_get_info(film_id):
+    return json.dumps(get_info_about_film_from_db(film_id), ensure_ascii=False)
+
+
+@app.route('/api/docs')
+def api_show_docs():
+    return render_template('api_page.html')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
